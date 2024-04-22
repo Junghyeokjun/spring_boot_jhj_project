45ab07ed2168bec1108c9681344d7565247c315e
admin <hyeokjun1218@gmail.com>
2024-04-22 16:17:49 +0900

    인메모리방식 유저생성

--- spring_boot_jhj_project/src/main/java/edu/sejong/ex/SecurityConfig.java
+++ spring_boot_jhj_project/src/main/java/edu/sejong/ex/SecurityConfig.java
@@ -1,6 +1,7 @@
 package edu.sejong.ex;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception {
 		
 //		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-		web.ignoring().antMatchers("/css/**","/js/**","/images/**","/lib/**");
+		web.ignoring().antMatchers("/css/**","/js/**","/imgx/**","/lib/**");
+	}
+	
+
+	//테스트용 유저 만들기(인메모리 방식)
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+		.withUser("member").password("{noop}member").roles("USER")
+		.and()
+		.withUser("admin").password("{noop}admin").roles("ADMIN");
+
+		
 	}
 }
