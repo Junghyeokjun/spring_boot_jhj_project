d52650575589cf04d322d473096ab498793c3969
admin <hyeokjun1218@gmail.com>
2024-04-25 16:45:52 +0900

    트랜잭션처리2

--- spring_boot_jhj_project/src/main/java/edu/sejong/ex/service/BoardServiceImpl.java
+++ spring_boot_jhj_project/src/main/java/edu/sejong/ex/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.sejong.ex.mapper.BoardMapper;
 import edu.sejong.ex.page.Criteria;
@@ -23,6 +24,7 @@ public class BoardServiceImpl implements BoardService{
 		return boardMapper.getList();
 	};
 	
+	@Transactional
 	@Override
 	public BoardVO get(int bno){
 		log.info("get()..");
@@ -47,7 +49,7 @@ public class BoardServiceImpl implements BoardService{
 		log.info("modifyBoard()..");
 		return boardMapper.updateBoard(boardVO);
 	}
-
+	@Transactional
 	@Override
 	public void writeReply(BoardVO boardVO) {
 		log.info("writeReply()..");
